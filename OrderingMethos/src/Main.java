4bf8932bf1c962bab7813ced3280cb3088689dab
Alexis8125 <laarenass@ufpso.edu.co>
2025-02-28 08:36:36 -0500

    Ordering Methods

---
+++ OrderingMethos/src/Main.java
@@ -0,0 +1,122 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int size = 10;
+        int[] array = generateRandomArray(size);
+
+        System.out.println("Arreglo original:");
+        printArray(array);
+
+        // Ordenamiento Burbuja
+        int[] bubbleSorted = array.clone();
+        bubbleSort(bubbleSorted);
+        System.out.println("Burbuja:");
+        printArray(bubbleSorted);
+
+        // Ordenamiento por Selección
+        int[] selectionSorted = array.clone();
+        selectionSort(selectionSorted);
+        System.out.println("Selección:");
+        printArray(selectionSorted);
+
+        // Ordenamiento por Inserción
+        int[] insertionSorted = array.clone();
+        insertionSort(insertionSorted);
+        System.out.println("Inserción:");
+        printArray(insertionSorted);
+
+        // QuickSort
+        int[] quickSorted = array.clone();
+        quickSort(quickSorted, 0, quickSorted.length - 1);
+        System.out.println("QuickSort:");
+        printArray(quickSorted);
+    }
+    
+    public static int[] generateRandomArray(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(100); // Números entre 0 y 99
+        }
+        return arr;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    // Ordenamiento Burbuja
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    // Ordenamiento por Selección
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            swap(arr, i, minIdx);
+        }
+    }
+
+    // Ordenamiento por Inserción
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // QuickSort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // Método para intercambiar elementos
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
